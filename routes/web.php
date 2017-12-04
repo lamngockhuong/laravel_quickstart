64f29ab13318b15c40a0e36455084053a163e6c6
Lâm Ngọc Khương <lamngockhuong@gmail.com>
2017-12-04 13:09:17 +0700

    tasks migrate and task model created

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,38 @@
 |
 */
 
+/**
+ * Show Task Dashboard
+ */
+
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+/**
+ * Add New Task
+ */
+
+Route::post('/task', function (Request $request) {
+    $validateData = $request->validate([
+        'name' => 'required|max:255',
+    ]);
+
+    // Create the Task
+    $task = new Task;
+    $task->name = $validateData['name']; // $request->name
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * Add Delete Task
+ */
+
+Route::delete('/task/{task}', function (Task $task) {
+
 });
