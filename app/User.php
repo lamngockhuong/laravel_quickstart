0e085af99913c3a55e212e90d499d02e70841001
Lam Ngoc Khuong <me@ngockhuong.com>
2017-12-12 15:28:07 +0700

    upgrading project

--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get all of the tasks of the user.
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
